991ea12a0e3d9976f910aaedeceb018f08fbb9eb
Aaron Friesen <aarongfriesen@gmail.com>
2020-04-26 15:19:07 -0500

    Initial commit

---
+++ Assets/NameGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class NameGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    string syllableNameGenerator(System.Random random, string[][] syllables)
+    {
+        // TODO implement
+        return null;
+    }
+}
